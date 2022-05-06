6175e8e7bbec5849769e15d8b6140dd97a57d557
Zhark10 <verygoodman18@yandex.ru>
2022-05-07 01:51:52 +0300

    add useSpeechRecognition

--- src/components/CardFooter/CardFooter.tsx
+++ src/components/CardFooter/CardFooter.tsx
@@ -1,18 +1,41 @@
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useEffect } from "react";
 import { Button } from "react-bootstrap";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './CardFooter.css';
 
 import { TranslatorContext } from "../../store/translator-context";
+import { ELanguage } from "../../store/translator-types";
 
 export const CardFooter: FC = () => {
-  const { actions } = useContext(TranslatorContext)
+  const { actions, state } = useContext(TranslatorContext)
+  const { transcript, listening } = useSpeechRecognition();
+
+  useEffect(function checkTranslateByVoice() {
+    if (!state.currentLanguage || !actions || !state.currentDictionary) return
+    const lang = {
+      [ELanguage.RUS]: ELanguage.ENG,
+      [ELanguage.ENG]: ELanguage.RUS,
+    }
+    const inactiveLanguage = lang[state.currentLanguage]
+    if (transcript === state.currentDictionary.currentWord[inactiveLanguage]) actions.nextWord()
+  }, [listening, transcript, state.currentLanguage, actions])
+
+  const listen = () => {
+    const language = state.currentLanguage === ELanguage.RUS ? "en" : "ru"
+    SpeechRecognition.startListening({language})
+    const resetByTime = setTimeout(SpeechRecognition.stopListening, 3000)
+    return () => clearTimeout(resetByTime)
+  }
 
   if (!actions) return <React.Fragment />
 
   return (
     <div className="Footer-button-groups">
-      <Button variant="primary" className="Footer-button" onClick={actions.showTranslate}>Показать перевод</Button>
-      <Button variant="primary" className="Footer-button" onClick={actions.nextWord}>Следующее слово</Button>
+      <div>
+        <Button variant="primary" className="Footer-button" onClick={actions.showTranslate}>Показать перевод</Button>
+        <Button variant="primary" className="Footer-button" onClick={actions.nextWord}>Следующее слово</Button>
+      </div>
+      <Button disabled={listening} variant="primary" className="Footer-button" onClick={listen}>{listening ? 'Говорите...' : 'Сказать перевод'}</Button>
     </div>
   )
 }
